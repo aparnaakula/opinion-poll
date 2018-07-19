a7964d0188d09cea6a8979075c55103feeadee0a
Aparna Akula <aparna.ramphe@gmail.com>
2018-07-19 10:15:31 -0500

    Added @SpringBootApplication annotation

--- opinion-poll/src/main/java/com/mycapstone/opinionpoll/OpinionPollApplication.java
+++ opinion-poll/src/main/java/com/mycapstone/opinionpoll/OpinionPollApplication.java
@@ -3,7 +3,7 @@ package com.mycapstone.opinionpoll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.mycapstone.opinionpoll" })
 public class OpinionPollApplication {
 
 	public static void main(String[] args) {
